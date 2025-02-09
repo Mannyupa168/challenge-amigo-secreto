be521ed7c395aafd220015ab620bcd1b217bfd45
Emmanuel Ortega <emmanuel.orteg98@gmail.com>
2025-02-09 00:07:16 -0600

    feat: add function to update friends list

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -3,14 +3,13 @@ let nombresAmigos = [];
 
 function agregarAmigo() {
   let nombreDeAmigo = document.getElementById('amigo').value;
-  console.log(nombreDeAmigo);
 
   if (nombreDeAmigo === '') {
     alert('Por favor, inserte un nombre');
     
   } else {
     nombresAmigos.push(nombreDeAmigo);
-    console.log(nombresAmigos);
+    actualizarListaDeAmigos();
     limpiarCampo();
   }
   return;
@@ -19,4 +18,16 @@ function agregarAmigo() {
 function limpiarCampo(){
   document.getElementById('amigo').value = '';
   return;
+}
+
+function actualizarListaDeAmigos() {
+  let listaDeAmigos = document.getElementById('listaAmigos');
+  listaDeAmigos.innerHTML = ''; //Limpia la lista de amigos
+  let elementoLista = ''; //Variable para construir la lista de amigos con el elemento <li>
+
+  for (let i = 0; i < nombresAmigos.length; i++) {
+    elementoLista += `<li>${nombresAmigos[i]}</li>`; // Agregar cada amigo dentro de <li>
+  }
+
+  listaDeAmigos.innerHTML = elementoLista; // Insertar los elementos en la lista
 }
