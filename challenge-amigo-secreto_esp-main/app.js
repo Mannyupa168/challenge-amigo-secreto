c91b37583f8f4cea51b48c7db6b36a28f70113d7
Emmanuel Ortega <emmanuel.orteg98@gmail.com>
2025-02-08 21:45:35 -0600

    feat: add function to add friends

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos = [];
+
+function agregarAmigo() {
+  let nombreDeAmigo = document.getElementById('amigo').value;
+  console.log(nombreDeAmigo);
+
+  if (nombreDeAmigo === '') {
+    alert('Por favor, inserte un nombre');
+    
+  } else {
+    nombresAmigos.push(nombreDeAmigo);
+    console.log(nombresAmigos);
+    limpiarCampo();
+  }
+  return;
+}
+
+function limpiarCampo(){
+  document.getElementById('amigo').value = '';
+  return;
+}
